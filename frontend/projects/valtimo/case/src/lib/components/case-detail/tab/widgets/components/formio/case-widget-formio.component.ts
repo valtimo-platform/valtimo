04699b629a94bb2d126995544796329b7ae594c7
mbritense <79840403+mbritense@users.noreply.github.com>
2025-07-08 14:53:24 +0200

    story: generalize widgets for iko (#1558)
    * widget collection generalized
    
    * widget table generalized
    
    * remove redudant styles
    
    * move initial number of elements logic collection
    
    * move initial number of elements logic table
    
    * remove redundant ng-container
    
    * migrate custom widget
    
    * pr comments

--- frontend/projects/valtimo/case/src/lib/components/case-detail/tab/widgets/components/formio/case-widget-formio.component.ts
+++ frontend/projects/valtimo/case/src/lib/components/case-detail/tab/widgets/components/formio/case-widget-formio.component.ts
@@ -91,4 +91,10 @@ export class CaseWidgetFormioComponent extends WidgetProcess implements OnInit {
   public onProcessStartClick(process: WidgetAction): void {
     this.widgetsService.startProcess(process.processDefinitionKey);
   }
+
+  public dataFetchedCallBackFunction = () => {
+    this.widgetConfiguration$.pipe(take(1)).subscribe(config => {
+      this.layoutService.setWidgetWithExternalDataReady(config.uuid);
+    });
+  };
 }
