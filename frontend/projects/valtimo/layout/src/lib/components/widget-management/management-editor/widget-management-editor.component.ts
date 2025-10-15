6e6bc112a3da594cd78c702763c073a25f470588
teodor-ritense <teodor.dragoi@ritense.com>
2025-10-15 13:35:59 +0200

    Fix IKO test findings

--- frontend/projects/valtimo/layout/src/lib/components/widget-management/management-editor/widget-management-editor.component.ts
+++ frontend/projects/valtimo/layout/src/lib/components/widget-management/management-editor/widget-management-editor.component.ts
@@ -208,7 +208,10 @@ export class WidgetManagementEditorComponent {
     this.widgetManagementService
       .updateWidgetConfiguration(widgets)
       .pipe(take(1))
-      .subscribe(() => this.$dragAndDropDisabled.set(false));
+      .subscribe(() => {
+        this.$dragAndDropDisabled.set(false);
+        this._refresh$.next(null);
+      });
   }
 
   private deleteWidget(tabWidget: BasicWidget): void {
