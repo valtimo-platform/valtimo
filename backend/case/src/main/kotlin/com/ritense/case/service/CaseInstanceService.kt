7f79fb67d411b93742ed93084716d42861eaeced
Marijn Verbeek <marijn.verbeek@ritense.com>
2025-10-21 17:03:15 +0200

    Moved retrieving case settings to the search rather than querying once per toCaseListRowDto. Should help with performance.

--- backend/case/src/main/kotlin/com/ritense/case/service/CaseInstanceService.kt
+++ backend/case/src/main/kotlin/com/ritense/case/service/CaseInstanceService.kt
@@ -16,6 +16,7 @@
 
 package com.ritense.case.service
 
+import com.ritense.case.domain.CaseDefinitionSettings
 import com.ritense.case.domain.CaseListColumn
 import com.ritense.case.repository.CaseDefinitionListColumnRepository
 import com.ritense.case.web.rest.dto.CaseListRowDto
@@ -52,7 +53,10 @@ class CaseInstanceService(
         val newPageable = mutatePageable(caseListColumns, pageable)
 
         return documentSearchService.search(caseDefinitionName, searchRequest, newPageable)
-            .map { document -> toCaseListRowDto(document, caseListColumns) }
+            .let {
+                val caseSettings = caseDefinitionService.getCaseSettings(caseDefinitionName)
+                it.map { document -> toCaseListRowDto(document, caseListColumns, caseSettings) }
+            }
     }
 
     private fun mutatePageable(caseListColumns: Collection<CaseListColumn>, pageable: Pageable): PageRequest {
@@ -65,7 +69,11 @@ class CaseInstanceService(
         return PageRequest.of(pageable.pageNumber, pageable.pageSize, newSort)
     }
 
-    private fun toCaseListRowDto(document: Document, caseListColumns: List<CaseListColumn>): CaseListRowDto {
+    private fun toCaseListRowDto(
+        document: Document,
+        caseListColumns: List<CaseListColumn>,
+        caseSettings: CaseDefinitionSettings
+    ): CaseListRowDto {
         val paths = caseListColumns.map { it.path }
         val resolvedValuesMap = valueResolverService.resolveValues(document.id().id.toString(), paths)
 
@@ -73,11 +81,8 @@ class CaseInstanceService(
             CaseListRowDto.CaseListItemDto(caseListColumn.id.key, resolvedValuesMap[caseListColumn.path])
         }.toMutableList()
 
-        if (items.none { it.key == "assigneeFullName" }) {
-            val caseSettings = caseDefinitionService.getCaseSettings(document.definitionId().name())
-            if (caseSettings.canHaveAssignee) {
-                items.add(CaseListRowDto.CaseListItemDto("assigneeFullName", document.assigneeFullName()))
-            }
+        if (items.none { it.key == "assigneeFullName"} && caseSettings.canHaveAssignee) {
+            items.add(CaseListRowDto.CaseListItemDto("assigneeFullName", document.assigneeFullName()))
         }
 
         return CaseListRowDto(document.id().toString(), items)
