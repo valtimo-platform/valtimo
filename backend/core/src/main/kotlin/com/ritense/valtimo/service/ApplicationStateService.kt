5742496f32aaa467ed917c3e87307b16c04c3061
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2025-10-21 09:13:56 +0200

    Public method

--- backend/core/src/main/kotlin/com/ritense/valtimo/service/ApplicationStateService.kt
+++ backend/core/src/main/kotlin/com/ritense/valtimo/service/ApplicationStateService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2024 Ritense BV, the Netherlands.
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ class ApplicationStateService(
 
     @Order(Ordered.LOWEST_PRECEDENCE)
     @EventListener(ApplicationReadyEvent::class)
-    private fun setApplicationReady() {
+    fun setApplicationReady() {
         applicationReady = true
         logger.debug { "Application is ready" }
     }
