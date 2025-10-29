60d34b8c0731c0649058987bcfc4b897ffd2e893
Maarten van Toor <maarten.van.toor@ritense.com>
2025-10-29 16:44:24 +0100

    moved Note events to contract package

--- backend/contract/src/main/kotlin/com/ritense/valtimo/contract/event/NoteCreatedEvent.kt
+++ backend/contract/src/main/kotlin/com/ritense/valtimo/contract/event/NoteCreatedEvent.kt
@@ -1,46 +1,42 @@
+/*
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.ritense.valtimo.contract.event
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.ritense.valtimo.contract.audit.AuditEvent
+import com.ritense.valtimo.contract.audit.AuditMetaData
 import com.ritense.valtimo.contract.audit.utils.AuditHelper
 import com.ritense.valtimo.contract.utils.RequestHelper
 import java.time.LocalDateTime
 import java.util.UUID
 
-data class NoteCreatedEvent(
-    private val id: UUID,
-    private val origin: String,
-    private val occurredOn: LocalDateTime,
-    private val user: String,
-    private val documentId: UUID?,
+@JsonIgnoreProperties(value = ["noteContent", "noteCreatedByUserId", "noteCreateByUserFullName", "noteCreatedOn"])
+class NoteCreatedEvent(
+    id: UUID = UUID.randomUUID(),
+    origin: String = RequestHelper.getOrigin(),
+    occurredOn: LocalDateTime = LocalDateTime.now(),
+    user: String = AuditHelper.getActor(),
     val noteId: UUID,
-) : AuditEvent {
+    val noteDocumentId: UUID,
+    val noteContent: String,
+    val noteCreatedByUserId: String,
+    val noteCreatedByUserFullName: String,
+    val noteCreatedOn: LocalDateTime,
+) : AuditMetaData(id, origin, occurredOn, user), AuditEvent {
 
-    constructor(documentId: UUID, noteId: UUID) : this(
-        UUID.randomUUID(),
-        RequestHelper.getOrigin(),
-        LocalDateTime.now(),
-        AuditHelper.getActor(),
-        documentId,
-        noteId,
-    )
-
-    override fun getId(): UUID {
-        return id
-    }
-
-    override fun getOrigin(): String {
-        return origin
-    }
-
-    override fun getOccurredOn(): LocalDateTime {
-        return occurredOn
-    }
-
-    override fun getUser(): String {
-        return user
-    }
-
-    override fun getDocumentId(): UUID? {
-        return documentId
-    }
+    override fun getDocumentId(): UUID? = noteDocumentId
 }
