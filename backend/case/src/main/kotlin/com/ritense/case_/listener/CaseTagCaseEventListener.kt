fd4bf6cfcc47eab3d9247fd00c0a2378a1753093
Klaas Schuijtemaker <94360980+Klaas-Ritense@users.noreply.github.com>
2025-10-31 12:48:43 +0100

    #387 copy tags to new draft (#113)

---
+++ backend/case/src/main/kotlin/com/ritense/case_/listener/CaseTagCaseEventListener.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.case_.listener
+
+import com.ritense.authorization.annotation.RunWithoutAuthorization
+import com.ritense.document.service.CaseTagService
+import com.ritense.document.web.rest.dto.CaseTagCreateRequestDto
+import com.ritense.valtimo.contract.annotation.SkipComponentScan
+import com.ritense.valtimo.contract.event.CaseDefinitionCreatedEvent
+import com.ritense.valtimo.contract.event.CaseDefinitionPreDeleteEvent
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@Service
+@SkipComponentScan
+class CaseTagCaseEventListener(
+    private val service: CaseTagService
+) {
+
+    @RunWithoutAuthorization
+    @EventListener(CaseDefinitionCreatedEvent::class)
+    fun createCaseTags(event: CaseDefinitionCreatedEvent) {
+        if (event.duplicate) {
+            service.getCaseTags(event.basedOnCaseDefinitionId!!).forEach { tag ->
+                service.create(
+                    event.caseDefinitionId, CaseTagCreateRequestDto(
+                        key = tag.id.key,
+                        title = tag.title,
+                        color = tag.color,
+                    )
+                )
+            }
+        }
+    }
+
+    @RunWithoutAuthorization
+    @EventListener(CaseDefinitionPreDeleteEvent::class)
+    fun handleCaseDefinitionPreDeleteEvent(event: CaseDefinitionPreDeleteEvent) {
+        service.getCaseTags(event.caseDefinitionId).forEach { tag ->
+            service.delete(event.caseDefinitionId, tag.id.key)
+        }
+    }
+
+}
