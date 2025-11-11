07cdb63530db0e0f7aea1912d4716ecb62d6beca
Maxim Buur <maxim.buur@ritense.com>
2025-11-11 15:20:01 +0100

    working front-end artwork

--- frontend/projects/valtimo/building-block-management/src/lib/components/building-block-management-artwork/building-block-management-artwork.component.ts
+++ frontend/projects/valtimo/building-block-management/src/lib/components/building-block-management-artwork/building-block-management-artwork.component.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {
   BuildingBlockManagementApiService,
@@ -25,11 +25,14 @@ import {ReadOnlyDirective, TooltipIconModule} from '@valtimo/components';
 import {
   ButtonModule,
   FileUploaderModule,
+  IconModule,
+  IconService,
   InputModule,
   LayerModule,
   LoadingModule,
 } from 'carbon-components-angular';
-import {BehaviorSubject, Subscription, tap} from 'rxjs';
+import {BehaviorSubject, switchMap, tap} from 'rxjs';
+import {TrashCan16, Upload16} from '@carbon/icons';
 
 @Component({
   standalone: true,
@@ -47,9 +50,10 @@ import {BehaviorSubject, Subscription, tap} from 'rxjs';
     ButtonModule,
     FileUploaderModule,
     LoadingModule,
+    IconModule,
   ],
 })
-export class BuildingBlockManagementArtworkComponent implements OnInit, OnDestroy {
+export class BuildingBlockManagementArtworkComponent {
   public readonly ACCEPTED_FILES: string[] = ['png'];
 
   public readonly formGroup = this.formBuilder.group({
@@ -60,27 +64,27 @@ export class BuildingBlockManagementArtworkComponent implements OnInit, OnDestro
     return this.formGroup.get('file') as FormControl<Set<File>>;
   }
 
-  private readonly _subscriptions = new Subscription();
-
   public readonly loadingArtwork$ = new BehaviorSubject<boolean>(true);
 
-  public readonly artwork$ = this.buildingBlockManagementApiService
-    .getBuildingBlockArtwork(
-      this.buildingBlockManagementDetailService.buildingBlockDefinitionKey,
-      this.buildingBlockManagementDetailService.buildingBlockDefinitionVersionTag
-    )
-    .pipe(tap(() => this.loadingArtwork$.next(false)));
+  private readonly _refresh$ = new BehaviorSubject<null>(null);
+
+  public readonly artwork$ = this._refresh$.pipe(
+    switchMap(() =>
+      this.buildingBlockManagementApiService.getBuildingBlockArtwork(
+        this.buildingBlockManagementDetailService.buildingBlockDefinitionKey,
+        this.buildingBlockManagementDetailService.buildingBlockDefinitionVersionTag
+      )
+    ),
+    tap(() => this.loadingArtwork$.next(false))
+  );
 
   constructor(
     private readonly buildingBlockManagementDetailService: BuildingBlockManagementDetailService,
     private readonly buildingBlockManagementApiService: BuildingBlockManagementApiService,
-    private readonly formBuilder: FormBuilder
-  ) {}
-
-  public ngOnInit(): void {}
-
-  public ngOnDestroy(): void {
-    this._subscriptions.unsubscribe();
+    private readonly formBuilder: FormBuilder,
+    private readonly iconService: IconService
+  ) {
+    this.iconService.registerAll([Upload16, TrashCan16]);
   }
 
   public async onSave(): Promise<void> {
@@ -97,8 +101,25 @@ export class BuildingBlockManagementArtworkComponent implements OnInit, OnDestro
           imageBase64,
         }
       )
+      .subscribe({
+        next: () => {
+          this._refresh$.next(null);
+          this.formGroup.enable();
+        },
+        error: () => {
+          this.formGroup.enable();
+        },
+      });
+  }
+
+  public onDelete(): void {
+    this.buildingBlockManagementApiService
+      .deleteBuildingBlockArtwork(
+        this.buildingBlockManagementDetailService.buildingBlockDefinitionKey,
+        this.buildingBlockManagementDetailService.buildingBlockDefinitionVersionTag
+      )
       .subscribe(() => {
-        this.buildingBlockManagementDetailService.reload();
+        this._refresh$.next(null);
       });
   }
 
@@ -114,32 +135,4 @@ export class BuildingBlockManagementArtworkComponent implements OnInit, OnDestro
       reader.onerror = error => reject(error);
     });
   }
-  //
-  // private openBuildingBlockDefinitionSubscription(): void {
-  //   this._subscriptions.add(
-  //     this.buildingBlockManagementDetailService.buildingBlockDefinition$.subscribe(
-  //       buildingBlockDefinition => {
-  //         this.formGroup.setValue({
-  //           name: buildingBlockDefinition.name,
-  //           key: buildingBlockDefinition.key,
-  //           description: buildingBlockDefinition.description,
-  //         });
-  //       }
-  //     )
-  //   );
-  // }
-  //
-  // private openLoadingSubscription(): void {
-  //   this._subscriptions.add(
-  //     this.buildingBlockManagementDetailService.loadingDefinition$.subscribe(loadingDefinition => {
-  //       if (loadingDefinition) {
-  //         this.formGroup.disable();
-  //       } else {
-  //         this.name.enable();
-  //         this.description.enable();
-  //         this.formGroup.markAsPristine();
-  //       }
-  //     })
-  //   );
-  // }
 }
