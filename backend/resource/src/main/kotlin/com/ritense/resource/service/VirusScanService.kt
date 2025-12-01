c7dfe11004cbe235453f3772788b2cfe95ba65df
paul-ritense <paul.van.beukering@ritense.com>
2025-12-01 22:13:17 +0100

    single scan function

--- backend/resource/src/main/kotlin/com/ritense/resource/service/VirusScanService.kt
+++ backend/resource/src/main/kotlin/com/ritense/resource/service/VirusScanService.kt
@@ -1,9 +1,7 @@
 package com.ritense.resource.service
 
 import com.ritense.resource.domain.VirusScanResult
-import java.io.InputStream
 
 interface VirusScanService {
     fun scan(bytes: ByteArray): VirusScanResult
-    fun scan(inputStream: InputStream): VirusScanResult
 }
