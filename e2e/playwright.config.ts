d7d7a6ea35758e53172124a62649791b061d2dbb
teodor-ritense <teodor.dragoi@ritense.com>
2025-12-03 10:41:08 +0100

    Add happy flow plugin tests

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig, devices } from "@playwright/test";
-import { config } from "dotenv";
+import {defineConfig, devices} from '@playwright/test';
+import {config} from 'dotenv';
 // @ts-ignore
-import moment from "moment";
-import momentDurationFormatSetup from "moment-duration-format";
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
 
 momentDurationFormatSetup(moment);
 
 // Load env files
-config({ path: ".env.properties" });
+config({path: '.env.properties'});
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
   fullyParallel: true,
   // forbidOnly: true,
   retries: process.env.CI ? 3 : 0,
   workers: process.env.CI ? 1 : 1,
-  reporter: [["line"], ["html", { open: "on-failure" }]],
-  globalSetup: "./utils/globalSetup.ts",
-  globalTeardown: "./utils/teardown.ts",
+  reporter: [['line'], ['html', {open: 'on-failure'}]],
+  globalSetup: './utils/globalSetup.ts',
+  globalTeardown: './utils/teardown.ts',
   timeout: process.env.qa_timeout
     ? moment.duration(process.env.qa_timeout).asMilliseconds()
     : 90_000,
@@ -27,46 +27,47 @@ export default defineConfig({
     launchOptions: {
       headless: false,
       // headless: process.env.CI ? true : process.env.headlessMode === "true",
-      args: ["--start-maximized"],
+      args: ['--start-maximized'],
     },
     baseURL: process.env.qa_url,
-    screenshot: process.env.CI ? "only-on-failure" : "on",
-    video: "on",
+    screenshot: process.env.CI ? 'only-on-failure' : 'on',
+    video: 'on',
     trace: {
-      mode: process.env.CI ? "retain-on-failure" : "on",
+      mode: process.env.CI ? 'retain-on-failure' : 'on',
       snapshots: true,
       screenshots: true,
     },
-    permissions: ["clipboard-read", "clipboard-write"],
+    permissions: ['clipboard-read', 'clipboard-write'],
+    testIdAttribute: 'data-test-id',
   },
 
   projects: [
     {
-      name: "setup user",
+      name: 'setup user',
       testMatch: /.*user\.setup\.ts$/,
     },
     {
-      name: "user tests",
+      name: 'user tests',
       testMatch: /.*\.user\.spec\.ts$/,
       use: {
-        ...devices["Desktop Chrome"],
+        ...devices['Desktop Chrome'],
         viewport: {
           width: 1920,
           height: 1080,
         },
       },
-      dependencies: ["setup user"],
+      dependencies: ['setup user'],
     },
     {
-      name: "admin tests",
+      name: 'admin tests',
       testMatch: /.*\/(?!.*\.user\.).*\.spec\.ts$/,
       use: {
-        ...devices["Desktop Chrome"],
+        ...devices['Desktop Chrome'],
         viewport: {
           width: 1920,
           height: 1080,
         },
-        storageState: "playwright/.auth/uiState.json",
+        storageState: 'playwright/.auth/uiState.json',
       },
     },
   ],
--- e2e/tests/plugins/plugin.spec.ts
+++ e2e/tests/plugins/plugin.spec.ts
@@ -1,7 +1,6 @@
 import {test, expect} from '@playwright/test';
 import {PluginPage} from './page';
-import {beforeEach} from 'node:test';
-import {pluginTypes} from './plugin-config';
+import {pluginTestConfiguration, pluginTypes} from './plugin-config';
 
 test.use({storageState: undefined});
 
@@ -23,27 +22,80 @@ test.describe('Plugin management', () => {
   });
 
   test.afterAll(async () => {
+    await pluginPage.deleteAllTestPlugins();
     await context.close();
   });
 
-  test('Add all plugins', async () => {
-    for (const type of pluginTypes) {
+  test.describe('Succsess test', () => {
+    test('Add all plugins', async () => {
+      for (const type of pluginTypes) {
+        // Act
+        if (type === 'Besluiten API') continue;
+
+        await pluginPage.openWizard();
+        await pluginPage.selectPluginType(type);
+        await pluginPage.fillPluginForm(type);
+        await pluginPage.saveConfiguration();
+
+        // Assert
+        await pluginPage.assertPluginExists(pluginTestConfiguration[type].pluginIdentifier);
+      }
+    });
+
+    test('Add Besluiten API plugin', async () => {
       // Act
       await pluginPage.openWizard();
-      await pluginPage.selectPluginType(type);
-      await pluginPage.fillPluginForm(type);
+      await pluginPage.selectPluginType('Besluiten API');
+      await pluginPage.fillPluginForm('Besluiten API');
       await pluginPage.saveConfiguration();
 
       // Assert
-      await pluginPage.assertPluginCreated(type);
-    }
-  });
+      await pluginPage.assertPluginExists(
+        pluginTestConfiguration['Besluiten API'].pluginIdentifier
+      );
+    });
 
-  test('Delete Besluiten API plugin', async () => {
-    // Act
-    await pluginPage.deletePlugin('Besluiten API');
+    test('Duplicate Besluiten API plugin', async () => {
+      // Act
+      await pluginPage.duplicateConfigurationName(
+        'Test Besluiten API',
+        'besluitenApiConfigurationTitle'
+      );
 
-    // Assert
-    await pluginPage.assertPluginDeleted('Besluiten API');
+      // Assert
+      await pluginPage.assertPluginExists('Test Besluiten API - Test Duplicated');
+    });
+
+    test('Edit Besluiten API plugin through row click', async () => {
+      // Act
+      await pluginPage.editPluginRowClick(
+        'Test Besluiten API - Test Duplicated',
+        'besluitenApiConfigurationTitle',
+        'Test Edited Besluiten API Row Click'
+      );
+      // Assert
+      await pluginPage.assertPluginExists('Test Edited Besluiten API Row Click');
+    });
+
+    test('Edit Besluiten API plugin through menu click', async () => {
+      // Act
+      await pluginPage.editPluginMenuClick(
+        'Test Edited Besluiten API Row Click',
+        'besluitenApiConfigurationTitle',
+        'Test Edited Besluiten API Menu Click'
+      );
+      // Assert
+      await pluginPage.assertPluginExists('Test Edited Besluiten API Menu Click');
+
+      await pluginPage.deletePlugin('Test Edited Besluiten API Menu Click');
+    });
+
+    test('Delete Besluiten API plugin', async () => {
+      // Act
+      await pluginPage.deletePlugin(pluginTestConfiguration['Besluiten API'].pluginIdentifier);
+
+      // Assert
+      await pluginPage.assertPluginDeleted('Besluiten API');
+    });
   });
 });
