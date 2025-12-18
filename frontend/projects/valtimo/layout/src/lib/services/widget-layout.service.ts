34112c4cc7fabe3de46c088ab945de9cb51807b9
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2025-12-18 08:38:09 +0100

    IKO 223: Widget lazy loading

--- frontend/projects/valtimo/layout/src/lib/services/widget-layout.service.ts
+++ frontend/projects/valtimo/layout/src/lib/services/widget-layout.service.ts
@@ -68,13 +68,14 @@ export class WidgetLayoutService implements OnDestroy {
   public get loaded$(): Observable<boolean> {
     return combineLatest([
       this._widgetDataLoaded$,
+      this._widgets$,
       this._widgetsWithExternalData$,
       this._widgetsWithExternalDataReady$,
       this._containerWidth$,
     ]).pipe(
       map(
-        ([widgetDataLoaded, widgetsWithExternalData, widgetsWithExternalDataReady]) =>
-          widgetDataLoaded?.length >= 1 &&
+        ([widgetDataLoaded, widgets, widgetsWithExternalData, widgetsWithExternalDataReady]) =>
+          (widgetDataLoaded?.length >= 1 || widgets.length === 0) &&
           widgetsWithExternalData.length === widgetsWithExternalDataReady.length
       ),
       filter(loaded => !!loaded)
