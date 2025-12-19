22c28663fd173b7b008a3df0a3a7c8d1ac88bebd
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2025-12-19 20:33:39 +0100

    Fixed collection-widget height

--- frontend/projects/valtimo/layout/src/lib/components/widget-collection/widget-collection.component.ts
+++ frontend/projects/valtimo/layout/src/lib/components/widget-collection/widget-collection.component.ts
@@ -99,7 +99,13 @@ export class WidgetCollectionComponent implements AfterViewInit, OnDestroy {
       return;
     }
 
-    if (!this._initialNumberOfElements) this._initialNumberOfElements = value.numberOfElements;
+    if (!this._initialNumberOfElements) {
+      const widgetConfig = this.widgetConfiguration$.getValue();
+      this._initialNumberOfElements = Math.max(
+        value.numberOfElements,
+        widgetConfig?.properties?.defaultPageSize || 0
+      );
+    }
 
     let widgetData: Page<CollectionWidgetCardData> = value;
 
