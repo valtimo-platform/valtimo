cd083e0c3be0c7750bc747d0ebabbdb78032a1f6
teodor-ritense <teodor.dragoi@ritense.com>
2026-01-08 09:45:54 +0100

    Case switch versino tests

---
+++ e2e/tests/case-details-management/case-details-management.spec.ts
@@ -0,0 +1,66 @@
+import {expect, test} from '@playwright/test';
+import {CaseDetailsManagementPage} from './page';
+import * as ApiUtils from '../../utils/api.utils';
+import {caseConfiguration} from './case-config';
+import {expectErrorMessage} from '../../utils/ui.utils';
+import {CaseManagementUtils} from '../../utils/case.utils';
+
+test.use({storageState: undefined});
+
+test.describe('Case management', () => {
+  let context;
+  let page;
+  let caseDetailsManagementPage;
+  let request;
+
+  // Arrange
+  test.beforeAll(async ({browser, baseURL}) => {
+    // Create shared context & page
+    context = await browser.newContext({baseURL});
+    page = await context.newPage();
+    request = context.request;
+
+    caseDetailsManagementPage = new CaseDetailsManagementPage(page, request);
+
+    await page.goto('/');
+    await caseDetailsManagementPage.goToCaseDetailsManagement('bezwaar');
+  });
+
+  test.describe('Success test', () => {
+    test('Switch version via dropdown', async () => {
+      // Act
+      await caseDetailsManagementPage.switchCaseVersionViaDropdown('1.0.0');
+
+      // Assert
+      expect(page).toHaveURL('**/1.0.0/**');
+    });
+
+    test('Switch version via list', async () => {
+      // Act
+      await caseDetailsManagementPage.switchCaseVersionViaList('1.0.0');
+
+      // Assert
+      expect(page).toHaveURL('**/1.0.0/**');
+    });
+
+    test('Export case definition', async () => {
+      //Act
+      await caseDetailsManagementPage.switchCaseVersionViaDropdown('1.0.0');
+      const download = await caseDetailsManagementPage.exportCaseDefinition();
+
+      //Assert
+      expect(download.suggestedFilename()).toContain('1.0.0');
+    });
+
+    // To fix
+    test('Set active version', async () => {
+      //Act
+      await caseDetailsManagementPage.makeVersionGlobal('1.0.0');
+
+      //Assert
+      expect(caseDetailsManagementPage.versionSelectDropdown.locator('button')).toContain(
+        'Globally active'
+      );
+    });
+  });
+});
