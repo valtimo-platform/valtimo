e84bd8194cd62878dec0189d0852a11908f88b3d
Maarten van Toor <maarten.van.toor@ritense.com>
2026-01-26 09:30:54 +0100

    migrated `RolNietNatuurlijkPersoonTest` to use AssertJ and enhanced exception message assertion

--- backend/zgw/zaken-api/src/test/kotlin/com/ritense/zakenapi/domain/rol/RolNietNatuurlijkPersoonTest.kt
+++ backend/zgw/zaken-api/src/test/kotlin/com/ritense/zakenapi/domain/rol/RolNietNatuurlijkPersoonTest.kt
@@ -1,5 +1,6 @@
 package com.ritense.zakenapi.domain.rol
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -7,8 +8,15 @@ internal class RolNietNatuurlijkPersoonTest {
 
     @Test
     fun `should throw exception when annIdentificatie, innNnpId, kvkNummer or vestigingsNummer is not provided`() {
-        assertThrows<IllegalArgumentException> {
-            RolNietNatuurlijkPersoon()
+        val exception = assertThrows<IllegalArgumentException> {
+            RolNietNatuurlijkPersoon(
+                annIdentificatie = null,
+                innNnpId = null,
+                kvkNummer = null,
+                vestigingsNummer = null
+            )
         }
+        assertThat(exception.message)
+            .isEqualTo("Either annIdentificatie, innNnpId, kvkNummer or vestigingsNummer should be provided!")
     }
 }
