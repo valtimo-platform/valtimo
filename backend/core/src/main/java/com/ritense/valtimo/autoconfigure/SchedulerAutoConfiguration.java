876f9f37fdc9a8fa956523320bb6fcd0c5a0ef0c
Maarten van Toor <maarten.van.toor@ritense.com>
2026-01-27 13:30:51 +0100

    updated `SchedulerAutoConfiguration` to allow configuration of `JdbcTemplateLockProvider` timezone via property, defaults to UTC

--- backend/core/src/main/java/com/ritense/valtimo/autoconfigure/SchedulerAutoConfiguration.java
+++ backend/core/src/main/java/com/ritense/valtimo/autoconfigure/SchedulerAutoConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2024 Ritense BV, the Netherlands.
+ * Copyright 2015-2026 Ritense BV, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,27 +18,33 @@ package com.ritense.valtimo.autoconfigure;
 
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
+import java.util.TimeZone;
 import javax.sql.DataSource;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
-import java.util.TimeZone;
 
 @AutoConfiguration
 @ConditionalOnClass(DataSource.class)
 public class SchedulerAutoConfiguration {
 
-    @Order(HIGHEST_PRECEDENCE + 13)
     @Bean
-    public LockProvider lockProvider(DataSource dataSource) {
+    @ConditionalOnMissingBean(LockProvider.class)
+    @Order(HIGHEST_PRECEDENCE + 13)
+    public LockProvider lockProvider(
+        final DataSource dataSource,
+        @Value("${timezone:UTC}") final String timeZone
+    ) {
         return new JdbcTemplateLockProvider(
             JdbcTemplateLockProvider.Configuration.builder()
                 .withJdbcTemplate(new JdbcTemplate(dataSource))
-                .withTimeZone(TimeZone.getTimeZone("UTC"))
+                .withTimeZone(TimeZone.getTimeZone(timeZone))
                 .build()
         );
     }
