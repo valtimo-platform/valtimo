af567104392085d8067cd57e0aa306a6abdcf2ac
Maxim Buur <maxim.buur@ritense.com>
2026-02-20 10:52:54 +0100

    merge next minor

--- documentation/release-notes/13.x.x/13.17.0/README.md
+++ documentation/release-notes/13.x.x/13.17.0/README.md
@@ -12,4 +12,9 @@
 
 * **Fix Formio Data Source URL**
 
-  The Formio Data Source URL now no longer contains an additional `/api`. 
+  The Formio Data Source URL now no longer contains an additional `/api`.
+
+- Fixed an issue where the Form.io custom component **Document Picker** did not show any files.
+- Fixed an issue where the Form.io custom component **Valtimo File Upload** failed to upload files.
+- Fixed file uploads so they now consistently use the correct **upload process** when uploading files.
+
--- frontend/projects/valtimo/components/src/lib/components/form-io/components/form-io-builder/form-io-builder.component.ts
+++ frontend/projects/valtimo/components/src/lib/components/form-io/components/form-io-builder/form-io-builder.component.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 Ritense BV, the Netherlands.
+ * Copyright 2015-2026 Ritense BV, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import {
   addValueResolverSelectorToEditform,
   modifyEditFormApiKeyInput,
 } from './form-io-builder.utils';
-import {ValtimoFormioOptions} from '../../../../models';
+import {FormioContextParams, ValtimoFormioOptions} from '../../../../models';
 import {deepmerge} from 'deepmerge-ts';
 import {isEqual} from 'lodash';
 import {
@@ -135,6 +135,7 @@ export class FormioBuilderComponent implements OnInit {
     );
 
     const originalEditForm = Components.baseEditForm;
+
     Components.baseEditForm = function (...extend) {
       const editForm = originalEditForm(...extend);
       modifyEditFormApiKeyInput(editForm);
