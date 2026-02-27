325affcbb70822b5c50b3ff924ac116933cadfa3
Maurits van Beusekom <maurits@vnbskm.nl>
2026-02-27 15:42:07 +0100

    Initial implementation of documenten-api-preview

---
+++ backend/zgw/documenten-api-preview/src/main/kotlin/com/ritense/documentenapipreview/DocumentenApiPreviewPlugin.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2015-2024 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ritense.documentenapipreview
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.ritense.documentenapi.DocumentenApiPlugin
+import com.ritense.documentenapipreview.DocumentenApiPreviewPlugin.Companion.PLUGIN_KEY
+import com.ritense.documentenapipreview.client.PdfConversionClient
+import com.ritense.plugin.annotation.Plugin
+import com.ritense.plugin.annotation.PluginProperty
+import com.ritense.plugin.service.PluginService
+import com.ritense.valtimo.contract.validation.Url
+import java.io.InputStream
+import java.net.URI
+
+
+@Plugin(
+    key = PLUGIN_KEY,
+    title = "Documenten API Preview",
+    description = "Allows previewing documents disclosed through the Documenten API"
+)
+class DocumentenApiPreviewPlugin(
+    private val pdfConversionClient: PdfConversionClient,
+    private val pluginService: PluginService,
+) {
+    @Url
+    @PluginProperty(key = URL_PROPERTY, secret = false)
+    lateinit var url: URI
+
+    @PluginProperty(key = DOCUMENTEN_API_CONFIGURATION_ID, secret = false)
+    lateinit var documentApiConfigurationId: String
+
+    fun generatePreview(documentId: String): InputStream {
+        val documentenApiPlugin = getDocumentenApiPlugin()
+        val documentStream = documentenApiPlugin.downloadInformatieObject(documentId)
+
+        return pdfConversionClient.convertDocument(url, documentStream)
+    }
+
+    private fun getDocumentenApiPlugin(): DocumentenApiPlugin {
+        return checkNotNull(pluginService.createInstance(documentApiConfigurationId)){
+            "Could not create instance of ${DocumentenApiPlugin::class.simpleName} based on documenten API configuration ID: $documentApiConfigurationId"
+        }
+    }
+
+    companion object {
+        const val PLUGIN_KEY = "documentenapipreview"
+        const val URL_PROPERTY = "url"
+        const val DOCUMENTEN_API_CONFIGURATION_ID = "documentenApiConfigurationId"
+
+        fun findConfigurationByDocumentenApiConfiguration(documentenApiConfigurationId: String) = { properties: JsonNode ->
+            documentenApiConfigurationId == properties[DOCUMENTEN_API_CONFIGURATION_ID].textValue()
+        }
+    }
+}
---
+++ backend/zgw/documenten-api-preview/src/main/kotlin/com/ritense/documentenapipreview/web/rest/DocumentenApiPreviewResource.kt
@@ -0,0 +1,38 @@
+package com.ritense.documentenapipreview.web.rest
+
+import com.ritense.documentenapi.service.DocumentenApiService
+import com.ritense.documentenapipreview.service.DocumentenApiPreviewService
+import com.ritense.logging.LoggableResource
+import com.ritense.plugin.domain.PluginConfiguration
+import com.ritense.valtimo.contract.annotation.SkipComponentScan
+import com.ritense.valtimo.contract.domain.ValtimoMediaType.APPLICATION_JSON_UTF8_VALUE
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.URLConnection
+
+@RestController
+@SkipComponentScan
+@RequestMapping("/api", produces = [APPLICATION_JSON_UTF8_VALUE])
+class DocumentenApiPreviewResource(
+    private val documentenApiPreviewService: DocumentenApiPreviewService
+) {
+    @GetMapping("/v1/documenten-api-preview/{pluginConfigurationId}/preview/{documentId}")
+    fun preview(
+        @LoggableResource(resourceType = PluginConfiguration::class) @PathVariable(name = "pluginConfigurationId") pluginConfigurationId: String,
+        @PathVariable(name = "documentId") documentId: String,
+    ): ResponseEntity<InputStreamResource> {
+
+        val previewStream = documentenApiPreviewService.generatePreview(pluginConfigurationId, documentId)
+
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.APPLICATION_PDF)
+            .body(InputStreamResource(previewStream))
+    }
+}
