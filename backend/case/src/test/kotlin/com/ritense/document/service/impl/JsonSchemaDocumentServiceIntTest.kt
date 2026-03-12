76b90d9390c3c570918d02c8f9894e056fea46c5
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-03-12 13:39:19 +0100

    609 Secure /users endpoints with PBAC

--- backend/case/src/test/kotlin/com/ritense/document/service/impl/JsonSchemaDocumentServiceIntTest.kt
+++ backend/case/src/test/kotlin/com/ritense/document/service/impl/JsonSchemaDocumentServiceIntTest.kt
@@ -101,7 +101,7 @@ internal class JsonSchemaDocumentServiceIntTest : BaseIntegrationTest() {
         val admin = ValtimoUser()
         admin.id = USERNAME
         admin.username = USERNAME
-        admin.roles = listOf(USER, ADMIN)
+        admin.setRoles(listOf(USER, ADMIN))
         whenever(userManagementService.currentUser).thenReturn(admin)
         whenever(userManagementService.findByUsername(USERNAME)).thenReturn(admin)
         whenever(userManagementService.findById(USERNAME)).thenReturn(admin)
