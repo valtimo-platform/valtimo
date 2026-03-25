8816844c25ddf4cc35bd1b88bde9b9099844d027
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-03-25 09:19:16 +0100

    refactor test

--- backend/building-block/src/test/kotlin/com/ritense/buildingblock/repository/JsonSchemaDocumentCaseDefinitionMapperIntegrationTest.kt
+++ backend/building-block/src/test/kotlin/com/ritense/buildingblock/repository/JsonSchemaDocumentCaseDefinitionMapperIntegrationTest.kt
@@ -16,19 +16,18 @@
 
 package com.ritense.buildingblock.repository
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.ritense.authorization.AuthorizationContext.Companion.runWithoutAuthorization
 import com.ritense.authorization.AuthorizationEntityMapperResult
 import com.ritense.buildingblock.BaseIntegrationTest
 import com.ritense.buildingblock.domain.instance.BuildingBlockInstance
-import com.ritense.buildingblock.service.BuildingBlockDefinitionDeploymentService
-import com.ritense.case.service.CaseDefinitionDeploymentService
+import com.ritense.buildingblock.service.BuildingBlockInstanceService
 import com.ritense.case_.domain.definition.CaseDefinition
 import com.ritense.document.domain.impl.JsonSchemaDocument
-import com.ritense.document.domain.impl.JsonSchemaDocumentDefinitionId
-import com.ritense.document.domain.impl.JsonSchemaDocumentId
+import com.ritense.document.domain.impl.request.NewDocumentRequest
 import com.ritense.document.repository.impl.JsonSchemaDocumentRepository
+import com.ritense.document.service.DocumentService
 import com.ritense.valtimo.contract.buildingblock.BuildingBlockDefinitionId
-import com.ritense.valtimo.contract.case_.CaseDefinitionId
-import java.time.LocalDateTime
 import java.util.UUID
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -40,41 +39,17 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class JsonSchemaDocumentCaseDefinitionMapperIntegrationTest @Autowired constructor(
     private val mapper: JsonSchemaDocumentCaseDefinitionMapper,
-    private val buildingBlockInstanceRepository: BuildingBlockInstanceRepository,
+    private val buildingBlockInstanceService: BuildingBlockInstanceService,
     private val jsonSchemaDocumentRepository: JsonSchemaDocumentRepository,
-    private val buildingBlockDefinitionDeploymentService: BuildingBlockDefinitionDeploymentService,
-    private val caseDefinitionDeploymentService: CaseDefinitionDeploymentService,
+    private val documentService: DocumentService,
+    private val objectMapper: ObjectMapper,
 ) : BaseIntegrationTest() {
 
     @Test
     fun `mapQuery should correctly link JsonSchemaDocument to CaseDefinition for building block blueprint`() {
-        buildingBlockDefinitionDeploymentService?.deployOnStartup()
-        caseDefinitionDeploymentService?.deployOnStartup()
+        val caseDocument = createCaseDocument()
+        val bbDocument = createBuildingBlockDocument(caseDocument.id().id)
 
-        val bbDefinitionId = BuildingBlockDefinitionId("bezwaar", Semver.parse("1.0.0")!!)
-
-        // Create a case document (needed as FK target for BuildingBlockInstance.caseDocumentId)
-        val caseDocument = createDocument(
-            JsonSchemaDocumentDefinitionId.forCase("bb-case", CaseDefinitionId("bb-case", Semver.parse("1.0.0")!!))
-        )
-
-        // Create building block document
-        val bbDocument = createDocument(
-            JsonSchemaDocumentDefinitionId.forBuildingBlock("bezwaar.schema", bbDefinitionId)
-        )
-
-        // Link building block document to case document via BuildingBlockInstance
-        val bbDefinition = buildingBlockDefinitionRepository.findById(bbDefinitionId).get()
-        val bbInstance = BuildingBlockInstance(
-            id = UUID.randomUUID(),
-            documentId = bbDocument.id().id,
-            caseDocumentId = caseDocument.id().id,
-            activityId = "activity-1",
-            definition = bbDefinition
-        )
-        buildingBlockInstanceRepository.save(bbInstance)
-
-        // Test mapQuery via Specification (mimics ContainerPermissionCondition behavior)
         val spec = Specification<JsonSchemaDocument> { root, query, cb ->
             val result: AuthorizationEntityMapperResult<CaseDefinition> = mapper.mapQuery(root, query!!, cb)
             val cd = result.root
@@ -97,28 +72,8 @@ class JsonSchemaDocumentCaseDefinitionMapperIntegrationTest @Autowired construct
 
     @Test
     fun `mapQuery should not find document if case name does not match`() {
-        buildingBlockDefinitionDeploymentService?.deployOnStartup()
-        caseDefinitionDeploymentService?.deployOnStartup()
-
-        val bbDefinitionId = BuildingBlockDefinitionId("bezwaar", Semver.parse("1.0.0")!!)
-
-        val caseDocument = createDocument(
-            JsonSchemaDocumentDefinitionId.forCase("bb-case", CaseDefinitionId("bb-case", Semver.parse("1.0.0")!!))
-        )
-
-        val bbDocument = createDocument(
-            JsonSchemaDocumentDefinitionId.forBuildingBlock("bezwaar.schema", bbDefinitionId)
-        )
-
-        val bbDefinition = buildingBlockDefinitionRepository.findById(bbDefinitionId).get()
-        val bbInstance = BuildingBlockInstance(
-            id = UUID.randomUUID(),
-            documentId = bbDocument.id().id,
-            caseDocumentId = caseDocument.id().id,
-            activityId = "activity-2",
-            definition = bbDefinition
-        )
-        buildingBlockInstanceRepository.save(bbInstance)
+        val caseDocument = createCaseDocument()
+        createBuildingBlockDocument(caseDocument.id().id)
 
         val spec = Specification<JsonSchemaDocument> { root, query, cb ->
             val result = mapper.mapQuery(root, query!!, cb)
@@ -137,24 +92,34 @@ class JsonSchemaDocumentCaseDefinitionMapperIntegrationTest @Autowired construct
         assertThat(foundDocuments).isEmpty()
     }
 
-    private fun createDocument(definitionId: JsonSchemaDocumentDefinitionId): JsonSchemaDocument {
-        val constructor = JsonSchemaDocument::class.java.getDeclaredConstructor()
-        constructor.isAccessible = true
-        val document = constructor.newInstance()
+    private fun createCaseDocument(): JsonSchemaDocument {
+        return runWithoutAuthorization {
+            documentService.createDocument(
+                NewDocumentRequest(
+                    "bb-case.schema",
+                    "bb-case",
+                    "1.0.0",
+                    objectMapper.readTree("{}")
+                )
+            ).resultingDocument().orElseThrow() as JsonSchemaDocument
+        }
+    }
 
-        val idField = JsonSchemaDocument::class.java.getDeclaredField("id")
-        idField.isAccessible = true
-        idField.set(document, JsonSchemaDocumentId.newId(UUID.randomUUID()))
-        document.setDefinitionId(definitionId)
-
-        val createdOnField = JsonSchemaDocument::class.java.getDeclaredField("createdOn")
-        createdOnField.isAccessible = true
-        createdOnField.set(document, LocalDateTime.now())
-
-        val createdByField = JsonSchemaDocument::class.java.getDeclaredField("createdBy")
-        createdByField.isAccessible = true
-        createdByField.set(document, "test-user")
-
-        return jsonSchemaDocumentRepository.save(document)
+    private fun createBuildingBlockDocument(caseDocumentId: UUID): JsonSchemaDocument {
+        return runWithoutAuthorization {
+            val bbInstance = buildingBlockInstanceService.create(
+                NewDocumentRequest(
+                    "bezwaar.schema",
+                    null,
+                    null,
+                    "bezwaar",
+                    "1.0.0",
+                    objectMapper.readTree("{}")
+                ),
+                caseDocumentId,
+                "activity-1",
+            )
+            documentService.get(bbInstance.documentId.toString()) as JsonSchemaDocument
+        }
     }
 }
