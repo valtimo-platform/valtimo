ebe10a7dc9835806a02081352b10296c0d8ab106
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-04 16:45:36 +0200

    remove unused

--- frontend/projects/valtimo/process-management/src/lib/components/process-management-builder/panel/valtimo-properties-provider.ts
+++ frontend/projects/valtimo/process-management/src/lib/components/process-management-builder/panel/valtimo-properties-provider.ts
@@ -65,9 +65,7 @@ class ValtimoPropertiesProvider {
         is(element, 'bpmn:StartEvent') ||
         is(element, 'bpmn:ServiceTask') ||
         is(element, 'bpmn:SendTask') ||
-        is(element, 'bpmn:ReceiveTask') ||
         is(element, 'bpmn:IntermediateThrowEvent') ||
-        is(element, 'bpmn:IntermediateCatchEvent') ||
         is(element, 'bpmn:CallActivity')
       ) {
         const customGroup = {
