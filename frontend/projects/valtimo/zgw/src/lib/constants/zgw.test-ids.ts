f188227426894eab6372134cd8e791e2c362b959
Sofia <131170700+sofiaIvarsRitense@users.noreply.github.com>
2026-04-24 14:10:14 +0200

    Playwright tests for ZGW document columns

--- frontend/projects/valtimo/zgw/src/lib/constants/zgw.test-ids.ts
+++ frontend/projects/valtimo/zgw/src/lib/constants/zgw.test-ids.ts
@@ -1,3 +1,10 @@
 export const ZGW_LINK_UPLOAD_PROCESS_TEST_IDS = {
   comboBox: 'linkUploadProcessComboBox',
 } as const;
+
+export const ZGW_DOCUMENT_COLUMNS_TEST_IDS = {
+  addButton: 'zgwDocumentColumnsAddButton',
+  modalColumnDropdown: 'zgwDocumentColumnsModalColumnDropdown',
+  modalSubmitButton: 'zgwDocumentColumnsModalSubmitButton',
+  modalCancelButton: 'zgwDocumentColumnsModalCancelButton',
+} as const;
