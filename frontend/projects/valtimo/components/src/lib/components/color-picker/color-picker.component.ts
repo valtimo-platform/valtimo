f98c6661711c652c9ed398ea07018943c3741d57
Sofia Ivars <sofia.ivars@ritense.com>
2026-05-08 10:14:57 +0200

    Reset to the default color on picker clear

--- frontend/projects/valtimo/components/src/lib/components/color-picker/color-picker.component.ts
+++ frontend/projects/valtimo/components/src/lib/components/color-picker/color-picker.component.ts
@@ -66,6 +66,7 @@ export class ColorPickerComponent
   @Input() public labelTranslationKey = 'colorPicker.label';
   @Input() public config: ColorPickerConfig = {};
   @Input() public i18n: ColorPickerI18n | null = null;
+  @Input() public defaultColor: string | null = null;
 
   @Output() public colorChangeEvent = new EventEmitter<string>();
 
@@ -207,6 +208,14 @@ export class ColorPickerComponent
     });
 
     this._pickr.on('clear', () => {
+      if (this.defaultColor) {
+        this._value = this.defaultColor;
+        this._pickr?.setColor(this.defaultColor);
+        this._onChangeFn(this.defaultColor);
+        this.colorChangeEvent.emit(this.defaultColor);
+        return;
+      }
+
       this._value = null;
       this._onChangeFn(null);
       this.colorChangeEvent.emit('');
