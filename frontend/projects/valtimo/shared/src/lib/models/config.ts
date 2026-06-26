1f29dda4db92d762992fd659bdf8e08bf742118f
Maxim Buur <maxim.buur@ritense.com>
2026-06-26 11:39:17 +0200

    move docu

--- frontend/projects/valtimo/shared/src/lib/models/config.ts
+++ frontend/projects/valtimo/shared/src/lib/models/config.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2025 Ritense BV, the Netherlands.
+ * Copyright 2015-2026 Ritense BV, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
@@ -93,12 +93,12 @@ interface ValtimoConfigFeatureToggles {
   enableSuppressDocumentError?: boolean;
   enableIkoType?: boolean;
   enableGenericCaseList?: boolean;
+  menuCollapsedByDefault?: boolean;
   /**
    * @deprecated DMN decision table editing is always enabled and is no longer gated by a
    * feature toggle. This option is ignored and will be removed in a future major release.
    */
   experimentalDmnEditing?: boolean;
-  menuCollapsedByDefault?: boolean;
 }
 
 interface ValtimoConfig {
