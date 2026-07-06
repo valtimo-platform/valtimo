c88fae74d951c5697e9816bd7f70da55741f28e4
Marijn Verbeek <marijn.verbeek@ritense.com>
2026-07-06 08:51:22 +0200

    changed opensearch to be disabled by default

--- backend/case-opensearch/src/main/kotlin/com/ritense/document/opensearch/OpenSearchProperties.kt
+++ backend/case-opensearch/src/main/kotlin/com/ritense/document/opensearch/OpenSearchProperties.kt
@@ -1,17 +1,19 @@
 /*
- * Copyright 2015-2024 Ritense BV, the Netherlands.
  *
- * Licensed under EUPL, Version 1.2 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2015-2026 Ritense BV, the Netherlands.
+ *  *
+ *  * Licensed under EUPL, Version 1.2 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" basis,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package com.ritense.document.opensearch
@@ -20,7 +22,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "valtimo.opensearch")
 data class OpenSearchProperties(
-    val enabled: Boolean = true,
+    val enabled: Boolean = false,
     val healthCheckEnabled: Boolean = true,
     val healthCheckIntervalMs: Long = 30000,
     val fallbackWarningIntervalMs: Long = 300000
