95ca91903df1d12c7e89a16a0f75aba70b5f6ca3
Marijn Verbeek <marijn.verbeek@ritense.com>
2026-07-07 14:56:11 +0200

    fixed integration tests for aws module

--- backend/aws/src/test/java/com/ritense/aws/parameterstore/AwsSsmPropertiesExtractorIntTest.java
+++ backend/aws/src/test/java/com/ritense/aws/parameterstore/AwsSsmPropertiesExtractorIntTest.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2015-2020 Ritense BV, the Netherlands.
  *
- * Licensed under EUPL, Version 1.2 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2015-2026 Ritense BV, the Netherlands.
+ *  *
+ *  * Licensed under EUPL, Version 1.2 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" basis,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package com.ritense.aws.parameterstore;
@@ -90,9 +92,12 @@ public class AwsSsmPropertiesExtractorIntTest extends BaseIntegrationTest {
         var awsSsmPropertiesExtractor = new AwsSsmPropertiesExtractor(parameterStoreConfiguration, ssmClient);
 
         //then
-        assertThat(propertySources).hasSize(1);
+        assertThat(propertySources).hasSize(3);
 
-        PropertySource<?> propertySource = propertySources.iterator().next();
+        PropertySource<?> propertySource = propertySources.stream()
+            .filter(ps -> ps.getName().contains("profile=" + profile))
+            .findFirst()
+            .orElseThrow();
         for (Map.Entry<String, String> testProperty : testProperties.entrySet()) {
             assertThat(propertySource.containsProperty(testProperty.getKey())).isTrue();
             assertThat(testProperty.getValue()).isEqualTo(propertySource.getProperty(testProperty.getKey()));
