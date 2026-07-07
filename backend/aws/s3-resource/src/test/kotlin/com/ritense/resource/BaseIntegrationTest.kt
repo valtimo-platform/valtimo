67e72c0ae6ba9d6f251c5dd4e35a4699e24c8d99
Marijn Verbeek <marijn.verbeek@ritense.com>
2026-07-07 16:18:20 +0200

    fixed integration tests for s3-resource module

--- backend/aws/s3-resource/src/test/kotlin/com/ritense/resource/BaseIntegrationTest.kt
+++ backend/aws/s3-resource/src/test/kotlin/com/ritense/resource/BaseIntegrationTest.kt
@@ -1,23 +1,26 @@
 /*
- * Copyright 2015-2020 Ritense BV, the Netherlands.
  *
- * Licensed under EUPL, Version 1.2 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  * Copyright 2015-2026 Ritense BV, the Netherlands.
+ *  *
+ *  * Licensed under EUPL, Version 1.2 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" basis,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
  *
- * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
  */
 
 package com.ritense.resource
 
 import com.ritense.resource.service.S3Service
 import com.ritense.valtimo.contract.authentication.UserManagementService
+import com.ritense.valtimo.contract.mail.MailSender
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
@@ -37,6 +40,9 @@ abstract class BaseIntegrationTest {
 
     @MockitoBean
     lateinit var userManagementService: UserManagementService
+
+    @MockitoBean
+    lateinit var mailSender: MailSender
 
     @MockitoBean
     lateinit var s3Client: S3Client
