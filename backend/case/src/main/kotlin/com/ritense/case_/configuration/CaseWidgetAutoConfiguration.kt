96d2f8cb66789d22e05d05242020b6cd0fb0d0e1
Maxim Buur <maxim.buur@ritense.com>
2026-07-09 09:23:34 +0200

    merge next-minor

--- backend/case/src/main/kotlin/com/ritense/case_/configuration/CaseWidgetAutoConfiguration.kt
+++ backend/case/src/main/kotlin/com/ritense/case_/configuration/CaseWidgetAutoConfiguration.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2024 Ritense BV, the Netherlands.
+ * Copyright 2015-2026 Ritense BV, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,7 +60,6 @@ import com.ritense.case_.widget.highlight.HighlightCaseWidgetDataProvider
 import com.ritense.case_.widget.highlight.HighlightCaseWidgetMapper
 import com.ritense.case_.widget.image.ImageCaseWidgetDataProvider
 import com.ritense.case_.widget.image.ImageCaseWidgetMapper
-import com.ritense.case_.widget.fieldsheader.FieldsCaseHeaderWidgetDataProvider
 import com.ritense.case_.widget.map.MapCaseWidgetDataProvider
 import com.ritense.case_.widget.map.MapCaseWidgetMapper
 import com.ritense.case_.widget.metroline.MetrolineCaseWidgetDataProvider
