9a75fdcb82b8fe16252a809fa25dc6aac39745b9
jarred-ritense <jarred.mac.intosh@ritense.com>
2026-08-05 15:10:39 +0200

    801 Added copyright and language seed.

--- frontend/projects/valtimo/components/src/lib/components/form-io/formio-flatpickr.ts
+++ frontend/projects/valtimo/components/src/lib/components/form-io/formio-flatpickr.ts
@@ -1,9 +1,25 @@
- import flatpickr from 'flatpickr';
+/*
+ * Copyright 2015-2025 Ritense BV, the Netherlands.
+ *
+ * Licensed under EUPL, Version 1.2 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import flatpickr from 'flatpickr';
 import {Dutch} from 'flatpickr/dist/l10n/nl';
 import {Formio} from 'formiojs';
 
 /**
- * Fromio only set the language on the first load.
+ * Formio only set the language on the first load.
  * This script can change the language of all active datepickers.
  * For all active datepickers, the new language is set and the component instance is reloaded.
  * The new language is immediately visible without a window reload.
